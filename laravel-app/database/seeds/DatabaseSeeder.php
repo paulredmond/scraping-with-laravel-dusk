d245e38dc320f805182de8d467fdd3d0daaa6594
Paul Redmond <paulrredmond@gmail.com>
2017-06-26 22:30:34 -0700

    Auth

--- laravel-app/database/seeds/DatabaseSeeder.php
+++ laravel-app/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory('App\User')->create([
+            'email' => 'paul@example.com',
+            'password' => bcrypt('secret'),
+        ]);
     }
 }
