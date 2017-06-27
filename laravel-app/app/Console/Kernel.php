c7450431b0ff2b9b7063092c98e10324578b369f
Paul Redmond <paulrredmond@gmail.com>
2017-06-26 22:30:59 -0700

    Disable scheduler

--- laravel-app/app/Console/Kernel.php
+++ laravel-app/app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('browser:download')->withoutOverlapping()->hourly();
-        $schedule->command('browser:extract')->withoutOverlapping()->everyMinute();
+        // $schedule->command('browser:extract')->withoutOverlapping()->everyMinute();
     }
 
     /**
